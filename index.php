bb96b06a7d152fb6ff797f743e13e56206661a12
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-02 21:30:46 +0700

    Trang Chủ

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
     //Chạy vào Trang Chủ
-    header("location: Site/trangchu.php");
+
+    // header("location: Site/trangchu.php");
+
+    require "./Dao/categories.php";
+    require "./global.php";
+
+    pdo_get_connect();
+
+    $dscate = category_selectall();
+
+    var_dump($dscate);
 ?>
