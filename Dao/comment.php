93a770c60c58bde4de899a4773fb43ebd9c90a2c
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-02 22:42:51 +0700

    Bình Luận
    Code bình luận

--- Dao/comment.php
+++ Dao/comment.php
@@ -3,4 +3,9 @@
         $sql = "SELECT * from postion";
         return pdo_query($sql);
     }
+
+    function position_insert($id){
+        $sql = "INSERT INTO position($id) VALUES('?')";
+        pdo_execute($sql,$id);
+    }
 ?>
