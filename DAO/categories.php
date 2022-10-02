938cf13c6f21d76c45a329152c996c70717decef
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-02 17:32:28 +0700

    Danh mục sản phẩm

--- DAO/categories.php
+++ DAO/categories.php
@@ -1,7 +1,18 @@
 <?php
+    // Gọi tất cả hàm
     function category_selectall(){
         $sql = "SELECT * from categories order by name";
         return pdo_query($sql);
+    }
+
+    function category_insert($id){
+        $sql = "INSERT INTO categories($id) VALUES('?')";
+        pdo_execute($sql,$id);
+    }
+
+    function category_delete($id){
+        $sql = "DELETE FROM categories WHERE id=?";
+        pdo_execute($sql,$id);
     }
     // update 
     function category_update(){
