9ee36b2ed4c22745a11409866197f14a3bc6f33e
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-02 22:38:02 +0700

    Khách hàng

--- DAO/users.php
+++ DAO/users.php
@@ -3,4 +3,9 @@
         $sql = "SELECT * from users";
         return pdo_query($sql);
     }
+
+    function user_insert($id){
+        $sql = "INSERT INTO users($id) VALUES('?')";
+        pdo_execute($sql,$id);
+    }
 ?>
