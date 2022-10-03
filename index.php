e607acdf5c494bac3dc8084be3dccbad59eb1f19
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-04 02:23:52 +0700

    Trang index

--- index.php
+++ index.php
@@ -1,35 +1,50 @@
 <?php
-require './DAO/categories.php';
-require './global.php';
+require_once "./global.php";
+require_once "./DAO/users.php";
+
+    $password ="TriChua18";
+    $fullname = "Nguyễn Trọng Trí";
+    $email = "caubelangthang2003@gmail.com";
+    $phone = "0385088031";
+    $address = "Hà Nội";
+    $id_position = "1";
+
+    // user_insert($password,$fullname,$email,$phone,$address,$id_position);
+
+    $data = user_selectall();
+
+    var_dump($data);
 //Thêm mới loại 
-if (isset($_POST['name'])) {
-    $name = $_POST['name'];
-    category_insert($name);
-}
+// if (isset($_POST['name'])) {
+//     $name = $_POST['name'];
+//     category_insert($name);
+// }
 
 
 // xóa
-if (isset($_GET['id'])) {
-    category_delete($_GET['id']);
-}
+// if (isset($_GET['id'])) {
+//     category_delete($_GET['id']);
+// }
 //load lại danh sách loại 
-$dsloai = category_selectall();
+
+// $dsloai = category_selectall();
 
 
 //var_dump($dsloai);
-foreach ($dsloai as $loai) {
-    extract($loai);
-    $del_link = "index.php?id=" . $id;
-    $edit_link = "edit.php?id=" . $id;
-    echo '<li> <a href="#">' . $name . '</a> - <a href="'.$del_link.'">del</a> - <a href="' .$edit_link.'">sửa</a></li>';
-}
+
+// foreach ($dsloai as $loai) {
+//     extract($loai);
+//     $del_link = "index.php?id=" . $id;
+//     $edit_link = "edit.php?id=" . $id;
+//     echo '<li> <a href="#">' . $name . '</a> - <a href="'.$del_link.'">del</a> - <a href="' .$edit_link.'">sửa</a></li>';
+// }
 ?>
 
 
-
+<!-- 
 
 <br> <br>
 <form action="index.php" method="post">
     <input type="text" name="name"> <br>
     <input type="submit" value="Thêm mới">
-</form>
+</form> -->
