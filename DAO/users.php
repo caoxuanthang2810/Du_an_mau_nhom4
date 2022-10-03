b65ead8dca89b1ddcc754c16c36ab226d63d6d52
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-04 02:24:18 +0700

    Chức năng

--- DAO/users.php
+++ DAO/users.php
@@ -1,11 +1,23 @@
 <?php
+    require_once "./global.php";
+
     function user_selectall(){
         $sql = "SELECT * from users";
         return pdo_query($sql);
     }
 
-    function user_insert($id){
-        $sql = "INSERT INTO users($id) VALUES('?')";
-        pdo_execute($sql,$id);
+    // function user_insert($id){
+    //     $sql = "INSERT INTO users($id) VALUES('?')";
+    //     pdo_execute($sql,$id);
+    // }
+
+    function user_insert($password,$fullname,$email,$phone,$address,$id_position){
+        $sql = "INSERT INTO users(password,fullname,email,phone,address,id_position) VALUES(?,?,?,?,?,?)";
+        pdo_execute($sql,$password,$fullname,$email,$phone,$address,$id_position);
+    }
+
+    function user_delete($id){
+        $sql = "delete from users where id = ?";
+        pdo_execute($sql, $id);
     }
 ?>
