d975eda5fedbc816869a75e5710e86ad740693d1
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-03 22:37:10 +0700

    Chức Năng

--- DAO/categories.php
+++ DAO/categories.php
@@ -1,26 +1,29 @@
 <?php
-
-    // Gọi tất cả hàm
+    // Gọi tất cả bảng
     function category_selectall(){
         $sql = "SELECT * from categories order by name";
         return pdo_query($sql);
     }
 
+    // Gọi hàm danh mục theo id
     function category_select_id($id){
         $sql = "SELECT * from categories id=?";
         return pdo_query($sql,$id);
     }
 
+    // Thêm mới danh mục
     function category_insert($name){
         $sql = "INSERT INTO categories(name) VALUES(?)";
         pdo_execute($sql,$name);
     }
 
+    // Xóa danh mục
     function category_delete($id){
-        $sql = "DELETE FROM categories WHERE id=$id";
+        $sql = "DELETE FROM categories WHERE id= $id";
         pdo_execute($sql,$id);
     }
-    // update 
+
+    // Update Danh mục 
     function category_update(){
         $sql = "UPDATE category SET name = '?' WHERE id = '?'";
         pdo_execute($sql);
