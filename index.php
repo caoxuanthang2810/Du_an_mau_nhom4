e4e52941b5527e0fd81cb5fce36a66bb0fda4425
Cao Xuân Thắng <114093877+caoxuanthang2810@users.noreply.github.com>
2022-10-03 23:01:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,20 @@
 <?php
-    //Chạy vào Trang Chủ
+if(isset($_POST['id'])){
+    category_update($_POST['id'],$_POST['name']);
+    header("location:index.php");
+}
+if(isset($_POST['id'])){
+     $id = $_POST['id'];
+      $update = get_categories($id);
+      extract($name); // lay ten loai
+?>
+<form action="./edit_loai.php" method="post">
+    <input type="text" name="name" value="<?=$name?>"><br>
+    <input type="hidden" name="id" value="<?=$id?>">
+    <button type="submit" name="gui">Cập nhật</button>
+</form>
 
-    // header("location: Site/trangchu.php");
-
-    require "./Dao/categories.php";
-    require_once "./global.php";
-    // Xóa
-    if(isset($_GET['id'])){
-        category_delete($_GET['id']);
-    }
-    $dscate = category_selectall();
-
-    foreach($dscate as $value){
-        extract($value);
-        $del_link = "index.php?id=".$id;
-        echo '<li><a href="#">'.$name.'</a> - <a href="'.$del_link.'">del</a></li>';
-
-
-    }
-
-    if(isset($_POST['btn_submit'])){
-
-        $name = $_POST['name'];
-        category_insert($name);
-    }
-
-    // lấy bản ghi theo id
-   
-
-
-    
-
-    
+<?php }
 ?>
 <form action="index.php" method="POST">
         <input type="text" name="name">
