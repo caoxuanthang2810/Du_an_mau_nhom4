ee1aff5bfcc68408ea86f126973444e832a539ae
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-10 01:05:00 +0700

    Giao Diện Khách Hàng

---
+++ Site/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    if(isset($_GET['danhmuc'])){
+        $VIEW_NAME = 'danhmuc.php';
+    }else if(isset($_GET['gioithieu'])){
+        $VIEW_NAME = 'gioithieu.php';     
+    }else if(isset($_GET['hoidap'])){
+        $VIEW_NAME = 'hoidap.php';   
+    }else if(isset($_GET['login'])){
+        $VIEW_NAME = 'login.php';   
+    }else if(isset($_GET['register'])){
+        $VIEW_NAME = 'register.php';   
+    }else if(isset($_GET['giohang'])){
+        $VIEW_NAME = 'giohang.php';   
+    }else{
+        $VIEW_NAME = 'trangchu.php';
+    }
+    include_once "./layout.php";
+
+
+?>
