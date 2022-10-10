d217cfd13423c57bf3c4db6a39e2e9c8768dc4ba
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-10 23:10:09 +0700

    Giao Diện Admin

--- Admin/products/add.php
+++ Admin/products/add.php
@@ -1,1 +1,28 @@
-<h2>Thêm sản phẩm</h2>
+<form action="" method="POST" enctype="multipart/form-data">
+<label for="">Tên sản phẩm</label>
+<input type="text" name="name"><br>
+<label for="">Ảnh sản phẩm</label>
+<input type="file" name="image"><br>
+<label for="">Giá sản phẩm</label>
+<input type="number" name="price"><br>
+<label for="">Mô tả sản phẩm</label>
+<input type="text" name="detail"><br>
+<label for="">Trạng thái</label>
+<select name="bit_active" id="">
+
+</select><br>
+<label for="">Mã màu</label>
+<select name="id_color" id="">
+
+</select><br>
+
+<label for="">Mã dung lượng</label>
+<select name="id_capacity" id="">
+
+</select><br>
+<label for="">Mã loại</label>
+<select name="id_categories" id="">
+
+</select><br>
+<button type="submit" name="gui">Thêm mới</button>
+</form>
