2bd4864457b648e3f92c9a072c6198248eee564c
Cao Xuân Thắng <114093877+caoxuanthang2810@users.noreply.github.com>
2022-10-11 01:01:13 +0700

    up giỏ hàng

---
+++ Site/giohang.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Giỏ Hàng</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="/public/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+    
+
+    <!-- Content -->
+
+    <div class="w-[1300px] bg-[#ebf2ff] h-[1000px] m-auto mt-10 py-4 rounded-2xl">
+    <div class="text-4xl text-center font-bold ">Giỏ hàng</div>
+    <div class="grid grid-cols-3 mt-[100px] text-2xl font-bold">
+      <div class="">Tên sản phẩm</div>
+      <div class="ml-[400px] w-[150px]">Trạng thái</div>
+      <div class="ml-[300px]">Giá</div>
+    </div>   
+    <hr class="border-b border-gray-600 w-[1300px] mt-[20px]  mb-[20px]">
+    <div class="">
+    <div class="grid grid-cols-4 mt-[30px] text-2xl font-bold">
+      
+      <img src="../Asset/img/content/Ip14.jpg" alt="" width="200px" height="200px" class="ml-[30px]">
+      <div class="ml-[100px] w-[250px] mt-[40px]">Điện thoại Iphone14 512GB- <samp>Màu</samp></div>
+      <div class="ml-[190px] mt-[50px]">Còn hàng</div>
+      <div class="ml-[150px] mt-[50px]">45.000.000đ</div>
+      <i class="fa-regular fa-circle-xmark ml-[100px] mt-[20px] text-4xl"></i>
+      
+      
+    </div>
+    <hr class="border-b border-gray-600 w-[1300px] mt-[20px]  mb-[20px]">
+    </div>
+    <div class="">
+    <div class="grid grid-cols-4 mt-[30px] text-2xl font-bold">
+      
+      <img src="../Asset/img/content/Ip14.jpg" alt="" width="200px" height="200px" class="ml-[30px]">
+      <div class="ml-[100px] w-[250px] mt-[40px]">Điện thoại Iphone14 512GB- <samp>Màu</samp></div>
+      <div class="ml-[190px] mt-[50px]">Còn hàng</div>
+      <div class="ml-[150px] mt-[50px]">45.000.000đ</div>
+      <i class="fa-regular fa-circle-xmark ml-[100px] mt-[20px] text-4xl"></i>
+      
+      
+    </div>
+    <hr class="border-b border-gray-600 w-[1300px] mt-[20px]  mb-[20px]">
+    </div>
+
+   <div class="flex">
+   <div class="w-[1100px] bg-amber-50 h-[50px] rounded-full flex">
+      <p class="text-3xl leading-[50px] ml-10 font-bold">Tổng:</p>
+      <p class="text-3xl font-bold leading-[50px] ml-[760px] text-blue-400">90.000.000đ</p>
+    </div>
+    <div class="w-[180px] bg-red-500 h-[50px] rounded-full ml-3 hover:bg-green-400">
+      <p class="text-xl leading-[50px] ml-10 font-bold">Thanh Toán</p>
+    </div>
+   </div>
+
+    </div>
+
+</body>
+</html>
