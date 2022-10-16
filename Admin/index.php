2a0d27ee7a77d65ec156f7a29264addc918b1faf
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-16 09:43:58 +0700

    Giao Diện Admin

--- Admin/index.php
+++ Admin/index.php
@@ -2,8 +2,12 @@
     require_once "./index.php";
 
     session_start();
+    
+    // var_dump($_SESSION["email"]);
+    // var_dump($_SESSION["id_position"]);
 
-    if(isset($_SESSION["email"])){
+
+    if(isset($_SESSION["email"]) && $_SESSION["email"] == "Admin@gmail.com"){
         header("location: ./dashboard/index.php");
     }else{
         echo "Bạn phải đăng nhập bằng quyền Admin thì mới vào được trang này";
