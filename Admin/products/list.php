32578f27278d60f32a7a626192c0c2683160a37e
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-23 00:22:04 +0700

    Giao Diện Admin

--- Admin/products/list.php
+++ Admin/products/list.php
@@ -105,7 +105,7 @@
                                         <?= $item['name_capacity'];?>
                                     </td>
                                     <td>
-                                        <?= $item['name_cate'];?>
+                                        <?= $name_cate?>
                                     </td>
                                     <td>
                                         <a class="border border-[#000] p-2 bg-[#008844] text-white" href="../products/index.php?update&id=<?=$id?>">Sửa</a>
