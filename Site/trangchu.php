169bfcc9241d08a17d8d975ddda7a69e5066eb67
TriChua18 <trintph20275@fpt.edu.vn>
2022-10-22 23:44:43 +0700

    Giao Diện Khách Hàng

--- Site/trangchu.php
+++ Site/trangchu.php
@@ -34,7 +34,7 @@ require_once "./index.php";
               <div class="mt-6 grid grid-cols-4 gap-4 bg-gray-300 ">
                 <?php foreach($item as $items):?>
                 <a href="./index.php?detail&id=<?=$items['id']?>"> <div class="text-center">
-                  <img class="w-full h-[300px]" src="<?=$items['image_file']?>" alt="">
+                  <img class="w-full h-[300px]" src="<?=$items['image']?>" alt="">
                 <div class="text-2xl font-normal hover:text-lime-500 "><?=$items['name']?></div>
                 <div class="text-red-500 font-bold">
                 <?=$items['price']?>
@@ -67,7 +67,7 @@ require_once "./index.php";
               <div class="mt-6 grid grid-cols-4 gap-4 bg-gray-300 ">
                 <?php foreach($pro as $items):?>
               <div class="text-center">
-                <a href="./index.php?detail&id=<?=$items['id']?>"><img class="w-full h-[300px]" src="<?=$items['image_file']?>" alt=""></a>
+                <a href="./index.php?detail&id=<?=$items['id']?>"><img class="w-full h-[300px]" src="<?=$items['image']?>" alt=""></a>
                 <a href="" class="text-2xl font-normal hover:text-lime-500 "><?=$items['name']?></a>
                 <div class="text-red-500 font-bold">
                 <?=$items['price']?>
